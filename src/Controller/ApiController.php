8193eecbd90f2534e6715617175508ec4a0479c9
vorwinator <vorwi111@gmail.com>
2022-04-06 19:03:08 +0200

    funkcja delete oraz zmiana $time na $date w funkcji update i post

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -31,10 +31,10 @@ class ApiController extends AbstractController
         
         $medicament->setNazwaLeku($param['nazwa_leku']);
         $medicament->setProducent($param['producent']);
-        $time = new DateTime($param['data_utworzenia']);
-        $medicament->setDataUtworzenia($time);
-        $time = new DateTime($param['data_modyfikacji']);
-        $medicament->setDataModyfikacji($time);
+        $date = new DateTime($param['data_utworzenia']);
+        $medicament->setDataUtworzenia($date);
+        $date = new DateTime($param['data_modyfikacji']);
+        $medicament->setDataModyfikacji($date);
 
         $em = $doctrine->getManager();
         $em->persist($medicament);
@@ -55,10 +55,10 @@ class ApiController extends AbstractController
 
         $data->setNazwaLeku($param['nazwa_leku']);
         $data->setProducent($param['producent']);
-        $time = new DateTime($param['data_utworzenia']);
-        $data->setDataUtworzenia($time);
-        $time = new DateTime($param['data_modyfikacji']);
-        $data->setDataModyfikacji($time);
+        $date = new DateTime($param['data_utworzenia']);
+        $data->setDataUtworzenia($date);
+        $date = new DateTime($param['data_modyfikacji']);
+        $data->setDataModyfikacji($date);
 
         $em = $doctrine->getManager();
         $em->persist($data);
@@ -66,6 +66,21 @@ class ApiController extends AbstractController
 
         return $this->json([
             "Updated succesfully!"
+        ]);
+    }
+
+
+    #[Route('/api/delete_api/{id}', name: 'delete_api', methods:"DELETE")]
+    public function delete_api(ManagerRegistry $doctrine, $id): Response
+    {
+        $data = $doctrine->getRepository(Lekarstwo::class)->find($id);
+
+        $em = $doctrine->getManager();
+        $em->remove($data);
+        $em->flush();
+
+        return $this->json([
+            "Deleted succesfully!"
         ]);
     }
 }
